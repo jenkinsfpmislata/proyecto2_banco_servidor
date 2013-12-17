9d7a89be00c147e93bd87df31dbcbf96f7d348b5
alumno <alumno@2A5PC22>
2013-12-17 09:40:49 +0100

    cuentaBancaria, hecho

---
+++ src/java/proyecto2/bank/datos/CuentaBancariaDAOImplHibernate.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2.bank.datos;
+
+import proyecto2.bank.negocio.CuentaBancaria;
+
+/**
+ *
+ * @author alumno
+ */
+public class CuentaBancariaDAOImplHibernate extends GenericDAOImplHibernate<CuentaBancaria,Integer> implements CuentaBancariaDAO  {
+    
+}
